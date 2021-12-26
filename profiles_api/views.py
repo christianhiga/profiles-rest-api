265178f0313cbae895537025016b4a4ebce5b833
Christin Higa <higachristian@gmail.com>
2021-12-26 01:04:15 -0300

    Added profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from profiles_api import serializers
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+
+from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 class HelloApiView(APIView):
 
     """Test API View"""
@@ -90,3 +94,11 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Habdle removing an object"""
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Habdle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_clases = (TokenAuthentication,)
+    permission_clases = (permissions.UpdateOwnProfile,)
